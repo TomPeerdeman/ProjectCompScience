50452a9550ca96da2eefa36ce2360a2f0adb5c58
Rene <rene66613@gmail.com>
2014-01-13 15:13:48 +0100

    triangular painted now, gridpaintchanger needed

--- ExtendedForestFire/src/main/java/nl/uva/ca/visual/forestfire/ExForestFirePanel.java
+++ ExtendedForestFire/src/main/java/nl/uva/ca/visual/forestfire/ExForestFirePanel.java
@@ -5,7 +5,6 @@ import java.awt.Graphics2D;
 
 import nl.tompeerdeman.ca.Grid;
 import nl.tompeerdeman.ca.visual.CaPanel;
-
 import nl.uva.ca.ExForestFireCell;
 import nl.uva.ca.ExForestFireCellType;
 
@@ -21,14 +20,53 @@ public class ExForestFirePanel extends CaPanel {
 	
 	@Override
 	public void paintTile(Graphics2D g, int x, int y) {
-		ExForestFireCell cell = (ExForestFireCell) grid.getCell(x, y);
-		// add more colors for different terrains
-		if(cell == null) {
-			g.setColor(Color.WHITE);
-		} else {
-			g.setColor(((ExForestFireCellType) cell.getType()).getColor());
-		}
-		
-		g.fillRect(x * dx + offsx, y * dy, dx, dy);
+		/*if( = 0){
+			ExForestFireCell cell = (ExForestFireCell) grid.getCell(x, y);
+
+			// add more colors for different terrains
+			if(cell == null) {
+				g.setColor(Color.WHITE);
+			} else {
+				g.setColor(((ExForestFireCellType) cell.getType()).getColor());
+			}
+			
+			g.fillRect(x * dx + offsx, y * dy, dx, dy);
+		}*/
+		//else{
+			ExForestFireCell cell = (ExForestFireCell) grid.getCell(x, y);
+	
+			// add more colors for different terrains
+			if(cell == null) {
+				g.setColor(Color.WHITE);
+			} else {
+				g.setColor(((ExForestFireCellType) cell.getType()).getColor());
+			}
+			if(y % 2 == 0){
+				if(x % 2 == 0){
+					int xPoly[] = {x * dx + offsx, x * dx + offsx + 2 * dx, x * dx + offsx + dx};
+					int yPoly[] = {y * dy, y * dy, y * dy + dy};
+					g.fillPolygon(xPoly, yPoly, 3);
+				}
+				else{
+					int xPoly[] = {x * dx + offsx, x * dx + offsx + 2 * dx, x * dx + offsx + dx};
+					int yPoly[] = {y * dy + dy, y * dy + dy, y * dy};
+					g.fillPolygon(xPoly, yPoly, 3);
+					
+				}
+			}
+			else{
+				if(x % 2 == 0){
+					int xPoly[] = {x * dx + offsx, x * dx + offsx + 2 * dx, x * dx + offsx + dx};
+					int yPoly[] = {y * dy + dy, y * dy + dy, y * dy};
+					g.fillPolygon(xPoly, yPoly, 3);
+				}
+				else{
+					int xPoly[] = {x * dx + offsx, x * dx + offsx + 2 * dx, x * dx + offsx + dx};
+					int yPoly[] = {y * dy, y * dy, y * dy + dy};
+					g.fillPolygon(xPoly, yPoly, 3);
+					
+				}
+			}
+		//}
 	}
 }
