707ff02a7a47594c91275b94d0e3a566e2016ad6
Rene <rene66613@gmail.com>
2014-01-16 20:03:58 +0100

    quickfix

--- ExtendedForestFire/src/main/java/nl/uva/ca/ExForestFireCell.java
+++ ExtendedForestFire/src/main/java/nl/uva/ca/ExForestFireCell.java
@@ -268,6 +268,8 @@ public class ExForestFireCell extends Cell {
 							// System.out.println("i wasnt null");
 							newCell.addFireFighter();
 							sim.addSimulatable(newCell);
+							// Remove FFighter from this cell.
+							return removeFireFighter(grid);
 						} else if(newCell == null) {
 							// System.out.println("i was null");
 							newCell =
@@ -277,10 +279,9 @@ public class ExForestFireCell extends Cell {
 							// New cell is FFighter only.
 							newCell.addFireFighter();
 							sim.addSimulatable(newCell);
+							// Remove FFighter from this cell.
+							return removeFireFighter(grid);
 						}
-						
-						// Remove FFighter from this cell.
-						return removeFireFighter(grid);
 					}
 				}
 				// System.out.println("");
